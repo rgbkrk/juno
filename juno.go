e094cf2ca1d6a4da1dbeb2955e4812eeaeb5b9b6
Kyle Kelley <rgbkrk@gmail.com>
2015-07-21 23:59:29 -0700

    No more main, we're a library now. :wink:

--- juno.go
+++ juno.go
@@ -7,9 +7,7 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"errors"
-	"flag"
 	"fmt"
-	"log"
 	"os"
 
 	zmq "github.com/pebbe/zmq4"
@@ -173,43 +171,4 @@ func NewIOPubSocket(connInfo ConnectionInfo, subscribe string) (*JupyterSocket, 
 	}
 
 	return iopub, nil
-}
-
-func main() {
-	flag.Parse()
-	if flag.NArg() < 1 {
-		log.Fatalln("Need a connection file.")
-	}
-
-	// Expects a runtime kernel-*.json
-	connInfo, err := OpenConnectionFile(flag.Arg(0))
-
-	if err != nil {
-		fmt.Errorf("%v\n", err)
-		os.Exit(1)
-	}
-
-	iopub, err := NewIOPubSocket(connInfo, "")
-
-	if err != nil {
-		fmt.Errorf("Couldn't start the iopub socket: %v", err)
-	}
-
-	defer iopub.Close()
-
-	for {
-		message, err := iopub.ReadMessage()
-
-		if err != nil {
-			fmt.Errorf("%v\n", err)
-			continue
-		}
-
-		c, err := json.Marshal(message)
-		if err != nil {
-			fmt.Println(string(c))
-		}
-
-	}
-
 }
